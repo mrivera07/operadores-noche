199021aca4c1439cdbc222ab0d46f838dcb31512
marcela rivera <yinar422@gmail.com>
2024-09-26 20:00:33 -0500

    se crea l proyecto con el archivo operadoresAritmeticos

---
+++ operdoresProject/operadoresDeAsignacion.py
@@ -0,0 +1,22 @@
+#Ejemplo con operadores de asignacion
+var1=5
+var2=12
+var3=8
+var4=14
+var5=6
+var6=8
+var1 +=3#var1 le esoy asignando var1+3
+print(var1)
+var1 -=3#var2 le esoy asignando var1+3
+var2 *=3
+var3 -=3
+var4 /=3
+var5 %=3    
+var6//=3
+
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+print(var5)
+print(var6)
