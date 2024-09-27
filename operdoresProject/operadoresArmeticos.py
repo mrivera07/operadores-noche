04b9acdea4708dc298b63476b2763bfeb691e8d3
marcela rivera <yinar422@gmail.com>
2024-09-26 19:47:22 -0500

    se crea l proyecto con el archivo operadoresAritmeticos

---
+++ operdoresProject/operadoresArmeticos.py
@@ -0,0 +1,17 @@
+#ejemplo operadores Aritmeticos
+#numero1:int=18
+#numero2:int=12
+numero1=int (input("ingrese el numero 1 :"))
+numero2=int (input("ingresa el numero 2:"))
+suma=numero1 + numero2
+print(f"La suma de {numero1} y {numero2} es:",suma)
+resta=numero1 - numero2
+division =numero1 / numero2
+multiplicacion=numero1 * numero2
+modulo=numero1%numero2
+divisionEntera=numero1// numero2
+print(f"la resta es:{numero1} y {numero2}:",resta)
+print(f"la division es :{numero1} y {numero2}:",division)
+print (f"la multiplicaion es:{numero1} y {numero2} ",multiplicacion)
+print(f"el modulo es:{numero1} y {numero2}",modulo)
+print(f"la division entera es:{numero1} y {numero2}",divisionEntera)
