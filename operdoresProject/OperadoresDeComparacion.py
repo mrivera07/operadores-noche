cae90dc1d2ae0d143a1d1b35dd04be5ab090782e
marcela rivera <yinar422@gmail.com>
2024-09-26 20:54:18 -0500

    se crea l proyecto con el archivo operadoresAritmeticos

---
+++ operdoresProject/OperadoresDeComparacion.py
@@ -0,0 +1,10 @@
+
+
+m=int(input("ingrese el valor de m "))
+n=int(input("ingrese el valor de n "))
+print(m==n)#igualdad
+print(m>n)#mayor que >
+print(m<n)#menor que
+print(m>=n)#mayor o igual que
+print(m<=n)#menor o igual que
+print(m!=n)#no igual
